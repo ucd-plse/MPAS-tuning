43f44e7b0deb4d8b1395b6d89f5a02ecdcd354eb
Jackson Vanover <jacksonvanover@gmail.com>
2023-06-15 14:00:09 -0600

    added back in the clean command to the build scripts for gnu and intel since the Makefiles to do not easily allow for incremental builds

--- scripts/build_MPAS_atm_intel.sh
+++ scripts/build_MPAS_atm_intel.sh
@@ -4,5 +4,5 @@ export PRECISION=double
 
 cd $(dirname "$0")/../
 source ./scripts/set_MPAS_env_intel.sh
-# make clean CORE=atmosphere
+make clean CORE=atmosphere
 make -j16 ifort CORE=atmosphere PRECISION="${PRECISION}"
