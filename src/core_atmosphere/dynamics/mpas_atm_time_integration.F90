712928d4a36b27f65915be32e7288729be942e95
Jackson Vanover <jacksonvanover@gmail.com>
2023-07-22 00:48:04 -0600

    Revert "added conditional check before using buffer"
    This reverts commit 460d5eab649c63b8e5d82c974e92eae3a1109c5d.

--- src/core_atmosphere/dynamics/mpas_atm_time_integration.F90
+++ src/core_atmosphere/dynamics/mpas_atm_time_integration.F90
@@ -4100,13 +4100,8 @@ module atm_time_integration
          tempField % next => null()
          tempField % isActive = .true.
 
-         if ( KIND(scale_arr_buffer) == KIND(scale_arr) ) then
-            tempField % array => scale_arr
-         else
-            scale_arr_buffer = scale_arr
-            tempField % array => scale_arr_buffer
-         end if
-         
+         scale_arr_buffer = scale_arr
+         tempField % array => scale_arr_buffer
          call mpas_dmpar_exch_halo_field(tempField, (/ 1 /))
 !$OMP END MASTER
 !$OMP BARRIER
