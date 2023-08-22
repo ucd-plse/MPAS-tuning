2eb68308c69bed9d056e54c2f9cbdf149c6e0537
Jackson Vanover <jacksonvanover@gmail.com>
2023-08-22 12:19:05 -0600

    removed deprecated rose build files

--- scripts/build_MPAS_atm_rose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PRECISION=double
-
-cd $(dirname "$0")/../
-source ./scripts/set_MPAS_env_rose.sh
-make clean CORE=atmosphere
-make -j1 rose-compiler CORE=atmosphere PRECISION="${PRECISION}"
