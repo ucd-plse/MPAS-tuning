c0cd6e131cc1ad774c5cf48817bd56bdf6a56ea3
Jackson Vanover <jacksonvanover@gmail.com>
2023-09-01 17:02:03 -0600

    set intel env to classic instead of oneapi as currently recommended by HPC staff

--- scripts/set_MPAS_env_intel.sh
+++ scripts/set_MPAS_env_intel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "## LOADING MPAS INTEL DEPENDENCIES"
 module reset
-module load intel-oneapi/2023.0.0
+module load intel-classic/2023.0.0
 module load parallel-netcdf/1.12.3
 module load parallelio/2.6.1
