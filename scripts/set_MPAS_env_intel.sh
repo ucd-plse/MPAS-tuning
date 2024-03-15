37493d7e20ec83e7ded8c5f57c8101d37b1aa569
Jackson Vanover <jacksonvanover@gmail.com>
2024-03-15 10:30:42 -0600

    updated derecho env script

--- scripts/set_MPAS_env_intel.sh
+++ scripts/set_MPAS_env_intel.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source /glade/u/home/jdvanover/precimonious-w-rose/scripts/derecho/activate_PROSE_environment.sh
+module load ncarenv/23.06
 module load intel-classic/2023.0.0
+module load cray-mpich/8.1.25
 module load parallel-netcdf/1.12.3
 module load parallelio/2.6.1
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/glade/u/home/jdvanover/gptl-8.1.1/install-intel/lib
